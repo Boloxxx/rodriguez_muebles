e86ca8cab6f1c5af6a03e41a69e1f4ffd23ce5e2
boloxxx <bryanrodriguezalmonte@gmail.com>
2024-04-20 22:17:05 -0400

    v.1

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+       
+       
+      </div>
+      <h1>Rodriguez Muebles</h1>
+      
+      <p className="read-the-docs">
+        <h3>Pagina Web en construccion</h3>
+      </p>
+    </>
+  )
+}
+
+export default App
